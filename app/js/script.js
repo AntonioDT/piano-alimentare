c2800270484074144ba4747bb2e06448b1e3b559
aditraglia <aditraglia@gmail.com>
2021-12-23 11:55:16 +0100

    ricerca

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,6 @@
 let accordion = document.getElementsByClassName("meal-title");
-
+let cards = document.getElementsByClassName("piano-alimentare")[0].children;
+var foodToSearch = "";
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
         let content = this.nextElementSibling;
@@ -12,4 +13,55 @@ for (let i = 0; i < accordion.length; i++) {
             icon[0].innerText = "-";
         }
     });
+}
+
+function searchFood(food) {
+    foodToSearch = food;
+}
+
+function filterFood() {
+    let cardsToShow = [];
+    if (foodToSearch || foodToSearch != "") {
+        Array.from(cards).forEach((card, index) => {
+            if (findFood(card)) {
+                cardsToShow.push(index);
+            }
+        });
+    }
+    showCards(cardsToShow);
+}
+
+function findFood(card) {
+    let found = false;
+    let foodGrid = card.getElementsByClassName("food-grid");
+    Array.from(foodGrid).forEach(grid => {
+        let elements = grid.getElementsByClassName("description");
+        Array.from(elements).forEach(description => {
+            let desc = description.textContent.toUpperCase();
+            if (desc.includes(foodToSearch.toUpperCase())) {
+                found = true;
+            }
+        });
+    });
+    return found;
+}
+
+function resetFood() {
+    foodToSearch = "";
+    let searchIinput = document.getElementsByClassName("search");
+    searchIinput[0].value = "";
+    Array.from(cards).forEach(card => {
+        card.classList.remove("hidden");
+    });
+}
+
+function showCards(cardsToShow) {
+    Array.from(cards).forEach(card => {
+        card.classList.add("hidden");
+    });
+    if (cardsToShow) {
+        Array.from(cardsToShow).forEach(i => {
+            cards[i].classList.remove("hidden");
+        });
+    }
 }
